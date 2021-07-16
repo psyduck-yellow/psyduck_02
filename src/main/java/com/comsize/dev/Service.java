f9669c19d2feab773198e6d0391cfe9b2d0289f7
mitsui <momota.private@gmail.com>
2021-07-16 14:08:13 +0900

    serviceクラスを作成

---
+++ src/main/java/com/comsize/dev/Service.java
@@ -0,0 +1,25 @@
+package com.comsize.dev;
+
+import java.util.Map;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@org.springframework.stereotype.Service
+public class Service {
+	@Autowired
+	private Repository repository;
+	
+	public Employee findOne(int id) {
+		Map<String, Object> map = repository.findOne(id);
+		
+		int employeeId = (Integer)map.get("employeeId");
+		String employeeName = (String)map.get("employeeName");
+		String employeeArea = (String)map.get("employeeArea");
+		
+		Employee employee = new Employee();
+		employee.setId(employeeId);
+		employee.setName(employeeName);
+		employee.setArea(employeeArea);
+
+		return employee; 
+	}
+}

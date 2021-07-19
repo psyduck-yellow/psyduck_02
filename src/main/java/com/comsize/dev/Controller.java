b637eab1337da71b8fad5a08ec3fa542c1cf2fe5
mitsui <momota.private@gmail.com>
2021-07-19 10:47:12 +0900

    コントローラクラスの作成

---
+++ src/main/java/com/comsize/dev/Controller.java
@@ -0,0 +1,58 @@
+package com.comsize.dev;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@org.springframework.stereotype.Controller
+public class Controller {
+
+    @Autowired
+    private Service Service;
+
+    /**
+     * GET用の処理.
+     */
+    @GetMapping("/hello")
+    public String getHello() {
+
+        // hello.htmlに画面遷移
+        return "hello";
+    }
+
+    /**
+     * POST用の処理.
+     */
+    @PostMapping("/hello")
+    public String postRequest(@RequestParam("text1") String str, Model model) {
+
+        // 画面から受け取った文字列をModelに登録
+        model.addAttribute("sample", str);
+
+        // helloResponse.htmlに画面遷移
+        return "Response";
+    }
+
+    /**
+     * POST用の処理（DB）.
+     */
+    @PostMapping("/hello/db")
+    public String postDbRequest(@RequestParam("ID") String str, Model model) {
+
+        // Stringからint型に変換
+        int id = Integer.parseInt(str);
+
+        // １件検索
+        Employee employee = Service.findOne(id);
+
+        // 検索結果をModelに登録
+        model.addAttribute("id", employee.getId());
+        model.addAttribute("name", employee.getName());
+        model.addAttribute("age", employee.getArea());
+
+        // helloResponseDB.htmlに画面遷移
+        return "helloResponseDB";
+    }
+}
